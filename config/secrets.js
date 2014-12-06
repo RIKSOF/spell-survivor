f9d369d4293fe8ae182a7f2fcb2db4a310619fae
riksof-abdulrauf <abdul.rauf@riksof.com>
2014-12-06 15:40:13 +0500

    Session secret change

--- config/secrets.js
+++ config/secrets.js
@@ -20,7 +20,7 @@ module.exports = {
 
   db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://admin:admin@ds061200.mongolab.com:61200/spell-survivor',
 
-  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
+  sessionSecret: process.env.SESSION_SECRET || 'TeamRiksofSessionSecret',
 
   mailgun: {
     user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
