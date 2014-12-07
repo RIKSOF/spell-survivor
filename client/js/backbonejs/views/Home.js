8b3a49045ac5686c2248e2a5701839018225b382
Sharjeel Khan <sharjeelkhan@github.com>
2014-12-07 17:43:58 +0500

    skip animation done

--- client/js/backbonejs/views/Home.js
+++ client/js/backbonejs/views/Home.js
@@ -1,4 +1,9 @@
 HomeView = Backbone.View.extend({
+	audioTheme : document.createElement('audio'),
+	timeOut: new Array(),
+	timeCount:0,
+	AnimateObj: new Array(),
+	AnimateCounter:0,
 	
 	initialize: function( options ){
 		this.render();
@@ -12,7 +17,22 @@ HomeView = Backbone.View.extend({
 	},
 	
 	events: {
-		"click .options li a":"clickOption"
+		"click .options li a":"clickOption",
+		"click .skipbutton":"skipAnimation"
+	},
+	
+	skipAnimation : function() {
+		this.audioTheme.pause();
+		$(".skipbutton").hide();
+		$.each( this.timeOut, function( key, value ) {
+ 			clearTimeout(value);
+		});
+		
+		$.each( this.AnimateObj, function( key, value ) {
+ 			value.stop();
+		});
+		
+		this.finishAnimation();
 	},
 	
 	clickOption: function (e) {
@@ -21,41 +41,8 @@ HomeView = Backbone.View.extend({
 		app.pubnubPublish(app.question.get("id"),selectOpt);
 	},
 	
-	initAnimation: function () {
-		//Audio theme start
-		var audioTheme = document.createElement('audio');
-        audioTheme.setAttribute('src', 'audio/theme.mp3');
-        audioTheme.setAttribute('autoplay', 'autoplay');
-        //audioElement.load()
-		
+	titleAnimate:function(){
 		var _home = this;
-		
-        $.get();
-
-        audioTheme.addEventListener("load", function() {
-            audioElement.play();
-        }, true);
-
-		audioTheme.play(); 
-		//Audio theme end
-        setTimeout(function(){
-			$("body").animate({opacity:1},2000);
-		},3000);
-		setTimeout(function(){
-			_home.$el.find('.river').animate({marginTop:"80px"},15000);
-			_home.$el.find('.mountainsmall').animate({height:"282px"},15000);
-			_home.$el.find('.mountainlarge').animate({height:"445px"},15000,function(){
-				_home.$el.find('.sky').animate({opacity:0},12000);
-				_home.$el.find('.sun').animate({top:"-500px",opacity:1},12000);
-				_home.$el.find('.mountainlarge').animate({opacity:1},12000);
-				_home.$el.find('.mountainsmall').animate({opacity:1},12000);
-				setTimeout(function(){
-					_home.$el.find('.mountainlargeshadow').animate({height:"104px"},9000);
-					_home.$el.find('.mountainsmallshadow').animate({height:"142px"},9000);
-				},3000);
-			});
-		},8500);
-		
 		var getFormData = function () {
 			var data = { 
 				loop: false, 
@@ -64,22 +51,24 @@ HomeView = Backbone.View.extend({
 			};
 			return data;
 		};
-		setTimeout(function(){
-			var $tlt = _home.$el.find('.logotext')
-			  .on('start.tlt', '')
-			  .on('inAnimationBegin.tlt', '')
-			  .on('inAnimationEnd.tlt', '')
-			  .on('outAnimationBegin.tlt', '')
-			  .on('outAnimationEnd.tlt', '')
-			  .on('end.tlt', '');
-			
-			var obj = getFormData();
-			$tlt.textillate(obj);
-		},35000);
-		setTimeout(function(){
-			audioTheme.pause();
-			_home.$el.find(".plank").animate({left:0},500,function(){
-				_home.$el.find(".timerMain").animate({top:0},500);
+		
+		var $tlt = _home.$el.find('.logotext')
+		  .on('start.tlt', '')
+		  .on('inAnimationBegin.tlt', '')
+		  .on('inAnimationEnd.tlt', '')
+		  .on('outAnimationBegin.tlt', '')
+		  .on('outAnimationEnd.tlt', '')
+		  .on('end.tlt', '');
+		
+		var obj = getFormData();
+		$tlt.textillate(obj);	
+	},
+	
+	loadPlank: function(){
+		var _home = this;
+		_home.$el.find(".cactus").animate({right:0},500);
+		_home.$el.find(".plank").animate({left:0},500,function(){
+		_home.$el.find(".timerMain").animate({top:0},500);
 				
 				//Quiz Voice start
 //				var audioElement = document.createElement('audio');
@@ -99,8 +88,73 @@ HomeView = Backbone.View.extend({
 				app.pubnubInit();
 				
 			});
-			_home.$el.find(".cactus").animate({right:0},500);
+	},
+	
+	finishAnimation: function(){
+		var _home = this;
+		_home.$el.find('.river').css({marginTop:"80px"});
+		_home.$el.find('.mountainsmall').css({height:"282px"});
+		_home.$el.find('.mountainlarge').css({height:"445px"});
+		_home.$el.find('.sky').css({opacity:0});
+		_home.$el.find('.sun').css({top:"-500px",opacity:1});
+		_home.$el.find('.mountainlarge').css({opacity:1});
+		_home.$el.find('.mountainsmall').css({opacity:1});
+		_home.$el.find('.mountainlargeshadow').css({height:"104px"});
+		_home.$el.find('.mountainsmallshadow').css({height:"142px"});
+		_home.titleAnimate();
+		setTimeout(function(){
+			_home.loadPlank();
+		},8000);
+	},
+	
+	initAnimation: function () {
+		//Audio theme start
+		
+        this.audioTheme.setAttribute('src', 'audio/theme.mp3');
+        this.audioTheme.setAttribute('autoplay', 'autoplay');
+        //audioElement.load()
+		
+		var _home = this;
+		
+        $.get();
+
+        this.audioTheme.addEventListener("load", function() {
+            this.audioTheme.play();
+        }, true);
+
+		this.audioTheme.play(); 
+		//Audio theme end
+        
+		_home.timeOut[_home.timeCounter++] = setTimeout(function(){
+			_home.AnimateObj[_home.AnimateCounter++] = $("body").animate({opacity:1},2000,function(){
+				
+				_home.timeOut[_home.timeCounter++] = setTimeout(function(){
+					_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.river').animate({marginTop:"80px"},15000);
+					_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.mountainsmall').animate({height:"282px"},15000);
+					_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.mountainlarge').animate({height:"445px"},15000,function(){
+						_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.sky').animate({opacity:0},12000);
+						_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.sun').animate({top:"-500px",opacity:1},12000);
+						_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.mountainlarge').animate({opacity:1},12000);
+						_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.mountainsmall').animate({opacity:1},12000);
+						_home.timeOut[_home.timeCounter++] = setTimeout(function(){
+							_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.mountainlargeshadow').animate({height:"104px"},9000);
+							_home.AnimateObj[_home.AnimateCounter++] = _home.$el.find('.mountainsmallshadow').animate({height:"142px"},9000);
+						},3000);
+					});
+				},3500);		
+			});
+		},3000);
+		
+		_home.timeOut[_home.timeCounter++] = setTimeout(function(){
+			_home.AnimateObj[_home.AnimateCounter++] = _home.titleAnimate();
+		},35000);
+		
+		
+		_home.timeOut[_home.timeCounter++] = setTimeout(function(){
+			_home.audioTheme.pause();
+			_home.AnimateObj[_home.AnimateCounter++] = _home.loadPlank();
 		},48000);
+		
 	}
 	
 });
