597dbd0f11793266fd0960cc0bb3c24db27a97df
Muhammad Zeeshan <riksof-zeeshan@github.com>
2014-12-07 13:01:21 +0500

    merged client

---
+++ client/js/backbonejs/models/Question.js
@@ -0,0 +1,17 @@
+Question = Backbone.Model.extend({
+	
+	initialize: function() {
+		this.on("change", function(model){
+			
+			var _attributes = model.attributes;
+			app.home.$el.find("#options1").html(_attributes.options[0]);
+			app.home.$el.find("#options2").html(_attributes.options[1]);
+			app.home.$el.find("#options3").html(_attributes.options[2]);
+			app.home.$el.find("#options4").html(_attributes.options[3]);
+			
+		});
+	}
+	
+	
+	
+});
---
+++ html/js/timer.js
@@ -0,0 +1,59 @@
+
+var counting = new (function() {
+    var $countdown,
+        $form, // Form used to change the countdown time
+        incrementTime = 70,
+        currentTime = 1575,
+        updateTimer = function() {
+            $countdown.html(formatTime(currentTime));
+            if (currentTime == 0) {
+                counting.Timer.stop();
+                timerComplete();
+                counting.resetCountdown();
+                return;
+            }
+            currentTime -= incrementTime / 10;
+            if (currentTime < 0) currentTime = 0;
+        },
+        timerComplete = function() {
+            //alert('Example 2: Countdown timer complete!');
+			$(".plank .options").hide();
+        },
+        init = function() {
+            $countdown = $('#countdown');
+			setTimeout(function(){
+            counting.Timer = $.timer(updateTimer, incrementTime, true);
+			},49000);
+        };
+    this.resetCountdown = function() {
+        var newTime = parseInt($form.find('input[type=text]').val()) * 100;
+        if (newTime > 0) {currentTime = newTime;}
+        this.Timer.stop().once();
+    };
+    $(init);
+});
+
+/**
+ * Example 4 is as simple as it gets.  Just a timer object and
+ * a counter that is displayed as it updates.
+ */
+var count = 0,
+    timer = $.timer(function() {
+        count++;
+        $('#counter').html(count);
+    });
+timer.set({ time : 1000, autostart : true });
+
+
+// Common functions
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {str = '0' + str;}
+    return str;
+}
+function formatTime(time) {
+    var min = parseInt(time / 6000),
+        sec = parseInt(time / 100) - (min * 60),
+        hundredths = pad(time - (sec * 100) - (min * 6000), 2);
+    return pad(sec, 2);
+}
